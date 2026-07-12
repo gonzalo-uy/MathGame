01a1a26d669c46cd75b797fa0cf52caf0283a25b
Jorge Betta <emus-ficticio-0t@icloud.com>
2026-07-12 12:07:56 -0300

    syntax error

--- Program.cs
+++ Program.cs
@@ -1,22 +1,15 @@
 ﻿using Spectre.Console;
+using MathGame;
 
-//Math operations menu
+
 while (true)
 {
-    Console.Clear();
-    var choice = AnsiConsole.Prompt(
-        new SelectionPrompt<MenuOption>()
-        .Title("Slect a game mode:")
-        .AddChoices(Enum.GetValues<MenuOption>()));
-    
-     var choiceLevel = AnsiConsole.Prompt(
-         new SelectionPrompt<LevelOption>()
-        .Title("Slect a level mode:")
-        .AddChoices(Enum.GetValues<LevelOption>()));
+  
+  MenuOption choice = UserInterface.ShowMenu();
 
     switch (choice)
     {
-        case MenuOption.Adition:
+        case MenuOption.Addition:
             break;
         case MenuOption.Subtraction:
             break;
@@ -24,14 +17,10 @@ while (true)
             break;
         case MenuOption.Multiplication:
             break;
-        case MenuOption:
+        case MenuOption.Random:
             break;
     }
 }
 
-//Level Menu
 
-   
 
-enum MenuOption { Adition, Subtraction, Division, Multiplication, Random };
-enum LevelOption {Easy, Normal, Hard};
--- UserInterface.cs
+++ UserInterface.cs
@@ -1,4 +1,27 @@
 using Spectre.Console;
+
 namespace MathGame;
 
+public class UserInterface
+{
+    public static MenuOption ShowMenu()
+    {
+        Console.Clear();
 
+        var choice = AnsiConsole.Prompt(
+       new SelectionPrompt<MenuOption>()
+       .Title("Slect a game mode:")
+       .AddChoices(Enum.GetValues<MenuOption>()));
+
+        return choice;
+    }
+}
+
+public enum MenuOption
+{
+    Addition,
+    Subtraction,
+    Division,
+    Multiplication,
+    Random
+};
